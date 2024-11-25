6658726fa28aebb0a66f99c694842cf8d23f9c92
Peter Zhou <zhoupete@amazon.com>
2024-11-24 22:09:59 -0500

    First version of commercial muter and add guide to readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-commercial-muter
+# Commercial Muter
+
+Mutes the TV when the game is in commercial. Optionally play music from Spoitfy when TV is muted.
+
+## Runbook
+1) `vim run-commercial-muter.sh`
+```
+export MATCH_TEMPLATE_PATH=<path/to/screenshot>
+export MATCH_THRESHOLD="0.75"
+export MUTE_DELAY="300"
+export DEVICE_ID="1e567bf4-8144-4576-8559-f8bba7ba2ba0_amzn_1"
+
+export SPOTIPY_CLIENT_ID=<your spotify app id>
+export SPOTIPY_CLIENT_SECRET=<your spotify app secret>
+export SPOTIPY_REDIRECT_URI="http://localhost:8888/callback/"
+
+python3 commercial-muter.py $1 $2
+```
+2) Connect a camera and point it at the bottom of your TV
+3) `./run-commercial-muter.sh`
+4) Take a screenshot of a piece of the scoreboard (team logo)
+5) Save the screenshot to /path/to/screenshot (configured in script)
+6) Restart the program
+
+#### Options:  
+* `-s` play Spotify when TV is muted (you'll need to create a Spotify app in developer mode and authenticate)
+* `-v` for verbose logging to help debug OpenCV matching
 
---
+++ commercial-muter.py
@@ -0,0 +1,110 @@
+import os
+import sys
+import time
+import requests
+import numpy as np
+import cv2 as cv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+
+def send_roku_command(command):
+    roku_ip = '10.88.111.12'
+
+    url = f'http://{roku_ip}:8060/keypress/{command}'
+    response = requests.post(url)
+    if response.status_code != 200:
+        print(f"Failed to send Mute/Unmute command. Status code: {response.status_code}")
+
+def pause_spotify(sp):
+    try:
+        sp.pause_playback(device_id=DEVICE_ID)
+    except Exception as e:
+        print(f"Pausing Spotify failed. {e}")
+
+def play_spotify(sp):
+    try:
+        sp.start_playback(device_id=DEVICE_ID)
+    except Exception as e:
+        print(f"Starting Spotify failed. {e}")
+
+def start_video():
+    cap = cv.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    return cap
+
+
+# Start script
+VERBOSE_LOG = '-v' in sys.argv
+SPOTIFY_ENABLED = '-s' in sys.argv
+
+MATCH_TEMPLATE_PATH = os.environ["MATCH_TEMPLATE_PATH"]
+MATCH_THRESHOLD = float(os.environ["MATCH_THRESHOLD"])
+MUTE_DELAY = float(os.environ["MUTE_DELAY"])
+DEVICE_ID = os.environ["DEVICE_ID"]
+
+if SPOTIFY_ENABLED:
+    scope = "user-library-read,user-read-playback-state,user-modify-playback-state,user-top-read"
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    print(sp.devices())
+
+template = cv.imread(MATCH_TEMPLATE_PATH, cv.IMREAD_GRAYSCALE)
+template_height, template_width = template.shape[:2]
+cap = start_video()
+
+is_game = True
+no_game_count = 0
+while True:
+    ret, frame = cap.read()
+    while not ret:
+        print("Cannot receive frame, trying to restart video capture.")
+        cap.release()
+        cv.destroyAllWindows()
+        time.sleep(8)
+        cap = start_video()
+        ret, frame = cap.read()
+
+    # try to match
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    result = cv.matchTemplate(gray, template, cv.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+    # rectangle around match
+    top_left = max_loc
+    bottom_right = (top_left[0] + template_width, top_left[1] + template_height)
+    cv.rectangle(frame, top_left, bottom_right, (0, 255, 0), 2)
+
+    # status changes
+    if max_val > MATCH_THRESHOLD:
+        if is_game:
+            if no_game_count > 0:
+                no_game_count = 0
+        else:
+            is_game = True
+            no_game_count = 0
+            print("Starting Game mode.")
+            if SPOTIFY_ENABLED:
+                pause_spotify(sp)
+            send_roku_command('VolumeMute')
+    else:
+        if VERBOSE_LOG:
+            print(f"No match. Rate: {max_val} < {MATCH_THRESHOLD}")
+
+        if is_game:
+            if no_game_count > MUTE_DELAY:
+                is_game = False
+                print(f"Starting Mute mode after {MUTE_DELAY // 10} seconds.")
+                if SPOTIFY_ENABLED:
+                    play_spotify(sp)
+                send_roku_command('VolumeMute')
+            else:
+                no_game_count += 1
+
+    cv.imshow('frame', frame)
+
+    if cv.waitKey(1) == ord('q'):
+        break
+ 
+cap.release()
+cv.destroyAllWindows()
